0ca812aba793f10e1d3e22f7db5738f51097101c
vstreamstack <76572685+vstreamstack@users.noreply.github.com>
2026-05-21 14:38:41 +0700

    Rename ExamplePlugin to AnichinPlugin

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -6,13 +6,13 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class ExamplePlugin: Plugin() {
+class AnichinPlugin: Plugin() {
     private var activity: AppCompatActivity? = null
 
     override fun load(context: Context) {
         activity = context as? AppCompatActivity
 
-        // All providers should be added in this manner
+        // Mendaftarkan pengeruk utama Anichin
         registerMainAPI(AnichinProvider())
     }
 }
